e64aaaf10058861ff7e705537ccb6b852afc0d7f
Weasel07z <moronsbob@gmail.com>
2023-04-22 00:27:25 -0500

    finally somthing actually changes stuff oml

--- js/game.js
+++ js/game.js
@@ -1,68 +1,101 @@
 var gameBoard;
 var gameOver = false;
+var cells = [[null, null, null, null],
+                [null, null, null, null],
+                [null, null, null, null],
+                [null, null, null, null]];
+var ids = [[1, 2, 3, 4],
+           [5, 6, 7, 8],
+           [9, 10, 11, 12],
+           [13, 14, 15, 16]];
 
-function Grid(size){
-    this.size = size;
-    this.cells = [];
-    this.create();
-    this.IDs = [];
-}
-Grid.prototype.create = function() {
-    var c = 1;
-    for (var i = 0; i < this.size; i++) {
-        var row = this.cells[i] = [];
-        var row2 = this.IDs[i] = [];
-        for(var j = 0; j < this.size; j++) {
-            row.push(null);
-            row2.push(c);
-            c++;
-        }
+class Grid {
+    constructor(size) {
+        this.size = size;
+        this.IDs = [];
     }
-};
-Grid.prototype.availableCells = function() {
-    var available = [];
-    for(var i = 0; i < this.size; i++){
-        for(var j = 0; j < this.size; j++) {
-            if(cells[i][j] == null){
-                available.push({x:i, y:j});
+    create() {
+        var c = 1;
+        for (var i = 0; i < this.size; i++) {
+            var row = cells[i] = [];
+            var row2 = this.IDs[i] = [];
+            for (var j = 0; j < this.size; j++) {
+                row.push(null);
+                row2.push(c);
+                c++;
             }
         }
     }
-    return available;
-};
-Grid.prototype.addRandomTile = function() {
-    if(!!this.availableCells().length){
-        var val = Math.random() < 0.9 ? 2 : 4;
-        var cell = this.availableCells[Math.random()*this.availableCells.length];
-        var tile = new Tile(IDs[cell.x][cell.y], cell.x, cell.y, val);
-        this.setCell(tile);
+    availableCells() {
+        var available = [];
+        for (var i = 0; i < this.size; i++) {
+            for (var j = 0; j < this.size; j++) {
+                if (cells[i][j] == null) {
+                    available.push({ x: i, y: j });
+                }
+            }
+        }
+        return available;
     }
-};
-Grid.prototype.initialTiles = function() {
-    for(var i = 0; i < 2; i++){
-        this.addRandomTile();
+    addRandomTile() {
+        if (!!this.availableCells().length) {
+            var val = Math.random() < 0.9 ? 2 : 4;
+            var cell = this.availableCells[Math.random() * this.availableCells.length];
+            const tile = new Tile(IDs[cell.x][cell.y], cell.x, cell.y, val);
+            this.setCell(tile);
+        }
     }
-};
-Grid.prototype.setCell = function(tile) {
-    this.cells[tile.x][tile.y] = tile;
-};
-Grid.prototype.getCell = function(cell) {
-    return this.cells[cell.x][cell.y];
-};
-Grid.prototype.removeTile = function(tile){
-    this.cells[tile.x][tile.y] = null;
-};
+    initialTiles() {
+        for (var i = 0; i < 2; i++) {
+            this.addRandomTile();
+        }
+    }
+    setCell(tile) {
+        cells[tile.x][tile.y] = tile;
+    }
+    getCell(cell) {
+        return cells[cell.x][cell.y];
+    }
+    removeTile(tile) {
+        cells[tile.x][tile.y] = null;
+    }
+}
 
 function startGame(){
-    var grid = new Grid(4);
-    grid.initialTiles();
+    initialize();
+    var c = 1;
+    for(var i = 0; i < 4; i++){
+        for(var j = 0; j < 4; j++){
+            if(cells[i][j] == null){
+                document.getElementById("cell" + c).innerHTML = 100;
+                c++;
+            } else {
+                document.getElementById("cell" + c).innerHTML = 200;
+                c++;
+            }
+        }
+    }
+}
+function initialize(){
+    var val = Math.random() < 0.9 ? 2 : 4;
+    var cellx = parseInt(Math.random() * 4, 10);
+    var celly = parseInt(Math.random() * 4, 10);
+    const tile = new Tile(ids[cellx][celly], cellx, celly, val);
+    cells[tile.x][tile.y] = tile;
 }
 function test3(){
     document.write("WORKS WORKS WORK SOWKRS WORKS");
 }
-function display(){
+function test2() {
+    document.getElementById("cell3").innerHTML = 10;
+}
+function displayGrid(){
+    //document.getElementById("cell2").innerHTML = this.getCell();
+
+    /*
     for(var x = 0; x < size; x++){
         for(var y = 0; y < size; y++){
+            
             if(cells[x][y] == null){
                 document.getElementById("cell1").innerHTML = 0;
             } else {
@@ -70,21 +103,26 @@ function display(){
             }
         }
     }
+    */
+    //document.getElementById("cell1").innerHTML = grid.getCell(grid.cells[2][2]);
 }
 
 // TILES STUFF * * * * * * * * * * * * *
-function Tile(id, x, y, value) {
-    this.id = id;
-    this.x = x;
-    this.y = y;
-    this.value = value;
-    this.color = black;
+class Tile {
+    constructor(id, x, y, value) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.value = value;
+        this.color = black;
+    }
+    updatePos(x, y) {
+        this.x = x;
+        this.y = y;
+        this.id = IDs[x][y];
+    }
+    getColor() {
+        return this.color;
+    }
 }
-Tile.prototype.updatePos = function(x, y){
-    this.x = x;
-    this.y = y;
-    this.id = IDs[x][y];
-};
-Tile.prototype.getColor = function(){
-    return this.color;
-};
+
