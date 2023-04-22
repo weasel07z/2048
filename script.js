0c11a4eb56a4878903513ba08c9ebafc2a5bc606
Weasel07z <moronsbob@gmail.com>
2023-04-21 20:40:04 -0500

    :3

--- script.js
+++ script.js
@@ -1,171 +1,72 @@
-var gameBoard = [][];
+var gameBoard = [];
 var gameOver = false;
+var curId = 0;
 
 function Grid(size){
     this.size = size;
     this.cells = [];
     this.create();
+    this.IDs = [];
 }
 Grid.prototype.create = function() {
-  for (var i = 0; i < this.size; i++) {
-    var row = this.cells[i] = [];
-    for (var j = 0; j < this.size; j++) {
-      row.push(null);
+    var c = 1;
+    for (var i = 0; i < this.size; i++) {
+        var row = this.cells[i] = [];
+        var row2 = this.IDs[i] = [];
+        for(var j = 0; j < this.size; j++) {
+            row.push(null);
+            row2.push(c);
+            c++;
+        }
     }
-  }
+};
+Grid.prototype.availableCells = function() {
+    var available = [];
+    for(var i = 0; i < this.size; i++){
+        for(var j = 0; j < this.size; j++) {
+            if(cells[i][j] == null){
+                available.push({x:i, y:j});
+            }
+        }
+    }
+    return available;
 };
 Grid.prototype.addRandomTile = function() {
-    if(!!this.availableCells.length)
-}
-Grid.prototype.initialTiles = function() {
-    for(var i = 0; i < 2; i++){
-        this.
+    if(!!this.availableCells().length){
+        var val = Math.random() < 0.9 ? 2 : 4;
+        var cell = this.availableCells[Math.random()*this.availableCells.length];
+        var tile = new Tile(IDs[cell.x][cell.y], cell.x, cell.y, val);
+        curId++;
+        this.setCell(tile);
     }
 };
-Grid.prototype.setTile = function(tile) {
+Grid.prototype.initialTiles = function() {
+    for(var i = 0; i < 2; i++){
+        this.addRandomTile();
+    }
+};
+Grid.prototype.setCell = function(tile) {
     this.cells[tile.x][tile.y] = tile;
 };
-Grid.prototype.getTile = function(cell) {
+Grid.prototype.getCell = function(cell) {
     return this.cells[cell.x][cell.y];
 };
 Grid.prototype.removeTile = function(tile){
     this.cells[tile.x][tile.y] = null;
+    curId--;
 };
 
-function Tile(id, x, y) {
-    this.num = 0;
+function Tile(id, x, y, value) {
     this.id = id;
     this.x = x;
     this.y = y;
+    this.value = value;
+    this.color = black;
 }
 Tile.prototype.updatePos = function(x, y){
     this.x = x;
     this.y = y;
 };
-Tile.prototype.getColor = function(value){
-    return #000000;
+Tile.prototype.getColor = function(){
+    return this.color;
 };
-/*var myGamePiece;
-var myObstacles = [];
-var myScore;
-
-function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 0.05;
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-    myGameArea.start();
-}
-
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
-        },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
-
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.score = 0;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;
-    this.gravity = 0;
-    this.gravitySpeed = 0;
-    this.update = function() {
-        ctx = myGameArea.context;
-        if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-        this.hitTop();
-    }
-    this.hitTop = function() {
-        var top = 0;
-        if(this.y < top){
-            this.y = top;
-            this.gravitySpeed = 0
-        }
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-        }
-    }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-}
-
-function updateGameArea() {
-    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            return;
-        } 
-    }
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "green", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    myScore.update();
-    myGamePiece.newPos();
-    myGamePiece.update();
-}
-
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}
-
-function accelerate(n) {
-    myGamePiece.gravity = n;
-}*/
