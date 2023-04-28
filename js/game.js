475ef7f8e6f037510c4ef07406c97ca0ae8b63fd
weasel07z <95734049+weasel07z@users.noreply.github.com>
2023-04-28 14:00:02 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -228,11 +228,13 @@ function getAvailableCells(){
            }
        }
     }
+    return available;
 }
 function spawnRandomTile(){
-        if (!!this.getAvailableCells().length) {
+    var avail = getAvailableCells();
+        if (!!avail.length) {
            var val = Math.random() < 0.9 ? 2 : 4;
-           var cell = this.getAvailableCells[Math.random() * this.getAvailableCells.length];
+           var cell = avail[Math.random() * avail.length];
            const tile = new Tile(cell.x, cell.y, val);
            cells[cell.y][cell.x]
         }
